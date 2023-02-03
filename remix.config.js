429c07891d5c182ca8922dedbc35c5d35b1e042e
Hodak <10237065+jhodak@users.noreply.github.com>
2023-02-03 11:13:00 -0500

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*", "**/*.php"],
+  ignoredRouteFiles: ["**/.*", "webhook.php"],
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
   serverBuildPath: "build/index.js",
